bbab4abb08dc373b009587cc11b5a737e1efbebf
ilyes <83750605+ilyesalili@users.noreply.github.com>
2023-01-24 03:36:00 +0100

    CRUD

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Data Posts</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+</head>
+<body style="background: lightgray">
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card border-0 shadow rounded">
+                    <h1 style="position: relative;display:flex;align-items: center;justify-content: center;">My post</h1>
+                    <div class="card-body">
+                        <a href="{{ route('posts.create') }}" class="btn btn-md btn-success mb-3">Add +</a>
+                        <table class="table table-bordered">
+                            <thead>
+                              <tr>
+                                <th scope="col">Image</th>
+                                <th scope="col">titre</th>
+                                <th scope="col">description</th>
+                                <th scope="col">modifier/delete</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              @forelse ($posts as $post)
+                                <tr>
+                                    <td class="text-center">
+                                        <img src="{{ Storage::url('public/posts/').$post->image }}" class="rounded" style="width: 150px">
+                                    </td>
+                                    <td>{{ $post->title }}</td>
+                                    <td>{!! $post->content !!}</td>
+                                    <td class="text-center">
+                                        <form onsubmit="return confirm('Vous êtes sûr ?');" action="{{ route('posts.destroy', $post->id) }}" method="POST">
+                                            <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-sm btn-primary">Modifier</a>
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-sm btn-danger">Delete</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                              @empty
+                                  <div class="alert alert-danger">
+                                  Les données relatives aux postes ne sont pas encore disponibles.
+                                  </div>
+                              @endforelse
+                            </tbody>
+                          </table>  
+                          {{ $posts->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+
+</body>
+</html>
